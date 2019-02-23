2588c8a4450032dda93047342545cc79eacad8e7
Dustin Oprea <doprea@magicleap.com>
2019-02-23 15:29:51 -0500

    gpsbabel_test.go: Add examples

--- gpsbabel_test.go
+++ gpsbabel_test.go
@@ -83,6 +83,54 @@ func TestBabel_Convert(t *testing.T) {
 	testGpxOutput(t, buffer)
 }
 
+func testPrintGpxData(r io.Reader) {
+	tpc := func(tp *gpxcommon.TrackPoint) (err error) {
+		fmt.Printf("%s (%.6f,%.6f)\n", tp.Time, tp.LatitudeDecimal, tp.LongitudeDecimal)
+
+		return nil
+	}
+
+	err := gpxreader.EnumerateTrackPoints(r, tpc)
+	log.PanicIf(err)
+}
+
+func ExampleBabel_Convert() {
+	b := NewBabel("v900", "gpx")
+
+	filepath := path.Join(TestAssetPath, "19020501_Portugal2.CSV.head")
+
+	f, err := os.Open(filepath)
+	log.PanicIf(err)
+
+	defer f.Close()
+
+	buffer := new(bytes.Buffer)
+
+	err = b.Convert(f, buffer)
+	if err != nil {
+		if log.Is(err, ErrConversionFailed) == true {
+			fmt.Printf("STDOUT:\n\n%s\n", buffer.String())
+		}
+
+		log.Panic(err)
+	}
+
+	// We need to parse the GPX so that the current timestamp that's placed into
+	// the GPX data doesn't destabilize the example.
+	testPrintGpxData(buffer)
+
+	// Output:
+	// 2019-02-05 08:07:05 +0000 UTC (38.760708,-9.112968)
+	// 2019-02-05 08:07:08 +0000 UTC (38.760738,-9.112924)
+	// 2019-02-05 08:07:09 +0000 UTC (38.760749,-9.112930)
+	// 2019-02-05 08:07:10 +0000 UTC (38.760753,-9.112923)
+	// 2019-02-05 08:07:14 +0000 UTC (38.760775,-9.112871)
+	// 2019-02-05 08:07:15 +0000 UTC (38.760775,-9.112861)
+	// 2019-02-05 08:07:16 +0000 UTC (38.760764,-9.112851)
+	// 2019-02-05 08:07:17 +0000 UTC (38.760754,-9.112848)
+	// 2019-02-05 08:07:18 +0000 UTC (38.760751,-9.112848)
+}
+
 func TestConvert(t *testing.T) {
 	filepath := path.Join(TestAssetPath, "19020501_Portugal2.CSV.head")
 
@@ -105,6 +153,39 @@ func TestConvert(t *testing.T) {
 	testGpxOutput(t, buffer)
 }
 
+func ExampleConvert() {
+	filepath := path.Join(TestAssetPath, "19020501_Portugal2.CSV.head")
+
+	f, err := os.Open(filepath)
+	log.PanicIf(err)
+
+	defer f.Close()
+
+	buffer := new(bytes.Buffer)
+
+	err = Convert("v900", "gpx", f, buffer)
+	if err != nil {
+		if log.Is(err, ErrConversionFailed) == true {
+			fmt.Printf("STDOUT:\n\n%s\n", buffer.String())
+		}
+
+		log.Panic(err)
+	}
+
+	testPrintGpxData(buffer)
+
+	// Output:
+	// 2019-02-05 08:07:05 +0000 UTC (38.760708,-9.112968)
+	// 2019-02-05 08:07:08 +0000 UTC (38.760738,-9.112924)
+	// 2019-02-05 08:07:09 +0000 UTC (38.760749,-9.112930)
+	// 2019-02-05 08:07:10 +0000 UTC (38.760753,-9.112923)
+	// 2019-02-05 08:07:14 +0000 UTC (38.760775,-9.112871)
+	// 2019-02-05 08:07:15 +0000 UTC (38.760775,-9.112861)
+	// 2019-02-05 08:07:16 +0000 UTC (38.760764,-9.112851)
+	// 2019-02-05 08:07:17 +0000 UTC (38.760754,-9.112848)
+	// 2019-02-05 08:07:18 +0000 UTC (38.760751,-9.112848)
+}
+
 func TestConvertToGpx(t *testing.T) {
 	filepath := path.Join(TestAssetPath, "19020501_Portugal2.CSV.head")
 
@@ -125,4 +206,37 @@ func TestConvertToGpx(t *testing.T) {
 	}
 
 	testGpxOutput(t, buffer)
+}
+
+func ExampleConvertToGpx() {
+	filepath := path.Join(TestAssetPath, "19020501_Portugal2.CSV.head")
+
+	f, err := os.Open(filepath)
+	log.PanicIf(err)
+
+	defer f.Close()
+
+	buffer := new(bytes.Buffer)
+
+	err = ConvertToGpx("v900", f, buffer)
+	if err != nil {
+		if log.Is(err, ErrConversionFailed) == true {
+			fmt.Printf("STDOUT:\n\n%s\n", buffer.String())
+		}
+
+		log.Panic(err)
+	}
+
+	testPrintGpxData(buffer)
+
+	// Output:
+	// 2019-02-05 08:07:05 +0000 UTC (38.760708,-9.112968)
+	// 2019-02-05 08:07:08 +0000 UTC (38.760738,-9.112924)
+	// 2019-02-05 08:07:09 +0000 UTC (38.760749,-9.112930)
+	// 2019-02-05 08:07:10 +0000 UTC (38.760753,-9.112923)
+	// 2019-02-05 08:07:14 +0000 UTC (38.760775,-9.112871)
+	// 2019-02-05 08:07:15 +0000 UTC (38.760775,-9.112861)
+	// 2019-02-05 08:07:16 +0000 UTC (38.760764,-9.112851)
+	// 2019-02-05 08:07:17 +0000 UTC (38.760754,-9.112848)
+	// 2019-02-05 08:07:18 +0000 UTC (38.760751,-9.112848)
 }
